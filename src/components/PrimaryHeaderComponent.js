879aa86cd4b2e91b857a8daf6eccd7ba2ff4a077
Vinu Devaraj <vinudevaraj007@gmail.com>
2021-06-27 19:29:32 +0530

    responsive primary header

--- src/components/PrimaryHeaderComponent.js
+++ src/components/PrimaryHeaderComponent.js
@@ -8,14 +8,17 @@ function PrimaryHeaderComponent() {
    return (
       <div className='primary-header-container '>
          <div className='container primary-header'>
-            <img className='soccerlive-icon' src={SoccerLive} alt='soccerlive logo' />
-            <div className='primary-header__search-container'>
+            <div className='primary-header__logo-container'>
+               <img className='soccerlive-icon' src={SoccerLive} alt='soccerlive logo' />
+               <span className='primary-header__span--large'>Soccerlive</span>
+            </div>
+            <div className='primary-header__search-input-container'>
                <AiOutlineSearch className='search-icon' />
-               <span>Search</span>
+               <input type='text' placeholder='Search' className='primary-header__search-input' />
             </div>
             <div className='primary-header__notification-container'>
                <img className='bell-icon' src={Bell} alt='bell icon' />
-               <span>Notifications</span>
+               <span className='primary-header__span--small'>Notifications</span>
             </div>
             <div className='hamburger'>
                <GiHamburgerMenu className='hamburger-icon' />
